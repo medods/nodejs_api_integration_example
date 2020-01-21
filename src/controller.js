351dce66d3e5e3ff9d92e73d9ce3dcca7a5a5327
Борис Безырганян <bezyrganyan@medods.ru>
2020-01-21 19:03:08 +0500

    singleton

--- src/controller.js
+++ src/controller.js
@@ -1,15 +1,14 @@
 const Router = require('koa-router')
 const JwtTokenService = require('./jwt_token_service')
 
-const jwtTokenService = new JwtTokenService()
 const router = new Router()
 
 router.get('/token', async ctx => {
-  ctx.body = await jwtTokenService.getToken()
+  ctx.body = await JwtTokenService.getInstance().getToken()
 })
 
 router.get('/update_token', async ctx => {
-  ctx.body = await jwtTokenService.updateToken()
+  ctx.body = await JwtTokenService.getInstance().updateToken()
 })
 
 module.exports = router
--- src/jwt_token_service.js
+++ src/jwt_token_service.js
@@ -1,21 +1,35 @@
 const jwt = require('jsonwebtoken')
-const {
-  identity,
-  secretKey,
-  tokenExpirationTime
-} = require('./configuration')
+const configuration = require('./configuration')
 
 const ALGORITHM = 'HS512'
 
 module.exports = class JwtTokenService {
-  constructor() {
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new JwtTokenService(
+        configuration.identity,
+        configuration.secretKey,
+        configuration.tokenExpirationTime
+      )
+      this.instance.initialize()
+    }
+
+    return this.instance
+  }
+
+  constructor(identity, secretKey, tokenExpirationTime) {
+    this.identity = identity
+    this.secretKey = secretKey
+    this.tokenExpirationTime = tokenExpirationTime
     this.token = null
     this.updateTokenPromise = null
 
     this.getToken = this.getToken.bind(this)
     this.updateToken = this.updateToken.bind(this)
+  }
 
-    const interval = (tokenExpirationTime / 2) * 1000
+  initialize() {
+    const interval = (this.tokenExpirationTime / 2) * 1000
     setInterval(this.updateToken, interval)
 
     this.updateToken()
@@ -32,15 +46,15 @@ module.exports = class JwtTokenService {
     this.token = null
     this.updateTokenPromise = new Promise((resolve, reject) => {
       // issuer
-      const iss = identity
+      const iss = this.identity
       // issued at
       const iat = Math.floor(Date.now() / 1000)
       // expiration
-      const exp = iat + tokenExpirationTime
+      const exp = iat + this.tokenExpirationTime
 
       jwt.sign(
         { iss, iat, exp },
-        secretKey,
+        this.secretKey,
         { algorithm: ALGORITHM },
         (err, token) => {
           if (err) reject(err)
@@ -55,6 +69,6 @@ module.exports = class JwtTokenService {
       .catch(err => console.error(err))
       .finally(() => { this.updateTokenPromise = null })
 
-    return this.updateTokenPromise
+    return this.getToken()
   }
 }
